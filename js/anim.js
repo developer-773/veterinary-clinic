bc180fcfd4702b9ae4b377d1622e6d2ed9395a25
Muhammadbek773 <galaxylogin@yandex.ru>
2022-07-20 17:00:34 +0500

    added modal js

---
+++ js/anim.js
@@ -0,0 +1,23 @@
+
+var modall = document.getElementById("mymodal");
+
+var btn = document.getElementById("myBtnn");
+
+var span = document.getElementsByClassName("close")[0];
+
+
+
+
+btn.onclick = function() {
+  modall.style.display = "block";
+}
+
+span.onclick = function() {
+  modall.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modall) {
+    modall.style.display = "none";
+  }
+}
